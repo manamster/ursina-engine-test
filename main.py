f23736e4a52671097c041118beef5da0e9d82bd1
manamaster <34355662+manamster@users.noreply.github.com>
2021-01-22 19:22:26 -0600

    Added a correct gun model and updated python file

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class Player(Entity):
         player = FirstPersonController(model="capsule.obj", y=0, origin_y=-.5)
         playerspeed = 10
         player.speed = playerspeed
-        gun = Entity(parent=camera, model='cube', x=0.5, z=1, y=-0.5, scale=Vec3(0.4, 0.2, 1), shader=lit_with_shadows_shader)
+        #Would import the model for the "gun" from the .blend file but the support apparently is broken :(
+        gun = Entity(parent=camera, model='gun.obj', x=0.7, z=1, y=-0.2, scale=Vec3(0.4,0.4,0.4), rotation=Vec3(0,-92,0),shader=lit_with_shadows_shader)
         player.gun = gun
     #This isnt working for some reason IDK Why Ill fix it later
     """ 
