76a6e4502dd66fd9a77671fc33864963b23fc972
manamaster <34355662+manamster@users.noreply.github.com>
2021-01-26 08:30:08 -0600

    Made it better i think???

--- main.py
+++ main.py
@@ -16,30 +16,26 @@ class World(object):
         monkey = Entity(model='monkey.obj', color=color.light_gray, shader=lit_with_shadows_shader, collider='box', y=1, x=-2)
         pivot=Entity()
         lightEntity = DirectionalLight(parent=pivot,y=2,z=3,shadowns=True)
-    
-class Player(Entity):
-    def __init__(self):
-        player = FirstPersonController(model="capsule.obj", y=0, origin_y=-.5)
-        playerspeed = 10
-        player.speed = playerspeed
+
+class Player(FirstPersonController):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.position=(2,1,0)
+        playerspeed=10
+        self.speed=playerspeed
         #Would import the model for the "gun" from the .blend file but the support apparently is broken :(
         gun = Entity(parent=camera, model='gun.obj', x=0.7, z=1, y=-0.2, scale=Vec3(0.4,0.4,0.4), rotation=Vec3(0,-92,0),shader=lit_with_shadows_shader)
-        player.gun = gun
-    #This isnt working for some reason IDK Why Ill fix it later
-    '''
+        self.gun = gun
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
     def input(self, key):
         if key == 'escape':
             sys.exit()
-        elif key == 'left mouse down' and player.gun:
-            gun.blink(color.red)
-        elif key == 'right mouse down' and player.gun:
-            gun.position(0,0,0)
-    '''
-    '''
+
     def update(self):
-        #test will eventually put stuff here
         pass
-    '''
+
 worldclass = World()
 playerclass = Player()
 
